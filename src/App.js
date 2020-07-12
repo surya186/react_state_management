efda8317972a474e80c9395eb95678cae4070570
Surya <kirantripurari9@gmail.com>
2020-07-12 16:54:28 +0530

    created Nav bar to handle the issues like passing useState data from parent to parent components. . 1) Solution are, i) Lifting up the state from MovieList to App and From App Nav can access state. ii) Main Solution is statemanagment ie., `context`

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import MovieList from "./MovieList";
+import Nav from './Nav'
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Nav /> <hr />
       <MovieList />
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+        <h3>State Management - React</h3>
+        <p> List of Movies: </p>
+    </div>
+  );
+};
+
+export default Nav;
